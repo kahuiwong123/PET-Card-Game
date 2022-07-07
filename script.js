81d1011c8192cea9b5952bf1ba714994b53500a7
Kahui <89529607+kahuiwong123@users.noreply.github.com>
2022-07-06 21:21:56 -0400

    Add files via upload
    updated login system

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Model
 let student_data;
-let all_students = [{name: "John Doe", class:"math", code:"H8W1"},
-                   {name: "Jane Doe", class:"english",code:"H8W2"},
-                   {name: "Tom", class:"art", code:"H8W3"}];
+let teacher_data = [{ username: "user", password: "pass" }];
+let teacher_username;
 
 const load_students = () => {
- const saved_students = JSON.parse(localStorage.getItem("students"));
+  const saved_students = JSON.parse(localStorage.getItem("students"));
   if (Array.isArray(saved_students) && saved_students.length > 0) {
     student_data = saved_students;
   } else {
@@ -18,29 +17,51 @@ load_students();
 const save_students = () => {
   localStorage.setItem("students", JSON.stringify(student_data));
 }
+
 // View
 const create_html = (html) => {
-    const template = document.createElement("template");
-    template.innerHTML = html.trim();
-    return template.content.firstElementChild;
+  const template = document.createElement("template");
+  template.innerHTML = html.trim();
+  return template.content.firstElementChild;
 }
 
 const student_inputs = () => {
-    const container = document.getElementById("student-container");
-    const student_form = document.getElementById("student-form");
-    if (typeof(student_form) === "undefined" || student_form === null) {
+  const container = document.getElementById("student-container");
+  const student_form = document.getElementById("student-form");
+  if (typeof (student_form) === "undefined" || student_form === null) {
     const inputs = create_html(`
     <div id="student-form">
         <input id="student_name" type="text" placeholder="Name">
         <input id="student_class" type="text" placeholder="Class">
         <input id="student_code" type="text" placeholder="Code">
-        <button class="student-submit" onclick="validate_student()">Login</button>
+        <button class="student-login" onclick="student_login()">Login</button>
     </div>`
     );
     container.appendChild(inputs);
-    } else {
-      container.removeChild(student_form);
-    }
+  } else {
+    container.removeChild(student_form);
+  }
+}
+
+const teacher_inputs = () => {
+  const container = document.getElementById("teacher-container");
+  const teacher_form = document.getElementById("teacher-form");
+  if (typeof (teacher_form) === "undefined" || teacher_form === null) {
+    const inputs = create_html(`
+    <div id="teacher-form">
+        <input id="teacher_user" type="text" placeholder="Username">
+        <input id="teacher_pass" type="text" placeholder="Password">
+        <button id="teacher-login" onclick="teacher_login()">Login</button>
+    </div>`
+    );
+    container.appendChild(inputs);
+  } else {
+    container.removeChild(teacher_form);
+  }
+}
+
+const render_add_student = () => {
+
 }
 
 const clear_textbox = (textbox) => {
@@ -48,76 +69,62 @@ const clear_textbox = (textbox) => {
 }
 
 // Controller
+
+/** 
 const add_student = () => {
-   const student_name = document.getElementById("student_name");
-   const student_class = document.getElementById("student_class");
-   const student_code = document.getElementById("student_code");
-   student_data.push({name:student_name.value,class:student_class.value, code:student_code.value});
-   save_students();
-   clear_textbox(student_name);
-   clear_textbox(student_class);
-   clear_textbox(student_code);
-   const container = document.getElementById("student-container");
-   const student_form = document.getElementById("student-form");
-   container.removeChild(student_form);
+  const student_name = document.getElementById("student_name");
+  const student_class = document.getElementById("student_class");
+  const student_code = document.getElementById("student_code");
+  student_data.push({ name: student_name.value, class: student_class.value, code: student_code.value });
+  save_students();
+  clear_textbox(student_name);
+  clear_textbox(student_class);
+  clear_textbox(student_code);
+  const container = document.getElementById("student-container");
+  const student_form = document.getElementById("student-form");
+  container.removeChild(student_form);
+} */
+
+const validate_student = (s_name, s_class, s_code) => {
+  return student_data.some(student => {
+    return student.name === s_name && student.class === s_class && student.code === s_code;
+  });
 }
 
-const validate_student = () => {
-    let valid = false;
-    const student_name = document.getElementById("student_name");
-    const student_class = document.getElementById("student_class");
-    const student_code = document.getElementById("student_code");
-    all_students.forEach(function(student){
-      console.log([student.name, student.class, student.code])
-      if (student.name === student_name.value && student.class === student_class.value && student.code === student_code.value) {
-        valid = true;
-      }
-    });
-  if(valid){
-    alert("login successful");
-    
-    return true;
-  }else{
-    alert("login unsuccessful");
-    return false;
+const validate_teacher = (user, pass) => {
+  return teacher_data.some(teacher => {
+    return teacher.username === user && teacher.password === pass;
+  });
+}
+
+const student_login = () => {
+  const student_name = document.getElementById("student_name");
+  const student_class = document.getElementById("student_class");
+  const student_code = document.getElementById("student_code");
+  if (student_name.value == "" || student_class.value == "" || student_code.value == "") {
+    alert("One of the fields is empty! Please try again.")
   }
-} 
-
-
-
-
-
-
-
-
-
-
-
-const teacher_inputs = () => {
-    const container = document.getElementById("teacher-container");
-    const teacher_form = document.getElementById("teacher-form");
-    if (typeof(teacher_form) === "undefined" || teacher_form === null) {
-    const inputs = create_html(`
-    <div id="teacher-form">
-        <input id="teacher_user" type="text" placeholder="Username">
-        <input id="teacher_pass" type="text" placeholder="Password">
-        <button class="teacher-login" onclick="teacher_login()">Login</button>
-    </div>`
-    );
-    container.appendChild(inputs);
-    } else {
-      container.removeChild(teacher_form);
-    }
+  else if (validate_student(student_name.value, student_class.value, student_code.value)) {
+    alert("Student login successful!");
+  } else {
+    alert("Wrong student login information! Please try again.");
+  }
 }
 
 const teacher_login = () => {
   const teacher_user = document.getElementById("teacher_user");
   const teacher_pass = document.getElementById("teacher_pass");
-  if(teacher_user.value === "user" && teacher_pass.value === "pass"){
-    alert("login successful");
-  }else{
-    alert("wrong login information");
+  const teacher_login_btn = document.getElementById("teacher-login");
+  if (teacher_user.value == "" || teacher_pass.value == "") {
+    alert("Username / Password cannot be empty! Please try again.");
   }
+  else if (validate_teacher(teacher_user.value, teacher_pass.value)) {
+    alert("Teacher login successful!");
+    teacher_username = teacher_user.value;
+    teacher_login_btn.onclick = window.location.href="Teacher/teacher.html"; 
+  } else {
+    alert("Wrong teacher login information! Please try again.");
+  };
 }
 
 
