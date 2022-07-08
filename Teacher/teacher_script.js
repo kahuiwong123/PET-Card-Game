bef6910b20054979bb0d3770214eca96caf9d600
Kahui <89529607+kahuiwong123@users.noreply.github.com>
2022-07-07 20:54:07 -0400

    Add files via upload

--- Teacher/teacher_script.js
+++ Teacher/teacher_script.js
@@ -4,19 +4,27 @@ const save_data = (key, data) => {
 }
 
 const add_student = (name, s_class, code) => {
-  student_data.push({name:name, class:s_class, code:code});
+  student_data.push({ name: name, class: s_class, code: code });
   save_data("students", student_data);
-  setTimeout(generate_table, 1);
+  alert("Student successfully added!");
 }
 
-const remove_all_students = () => {
-  student_data.length = 0;
+const delete_student = (button_name) => {
+  student_data = student_data.filter(student => {
+    if (student.code === button_name) {
+      alert(student.name + " has been removed");
+      return false;
+    } else {
+      return true;
+    }
+  });
   save_data("students", student_data);
 }
 
-const student_exists = (s_name) => {
+
+const code_exists = (code) => {
   return student_data.some(student => {
-    return student.name === s_name;
+    return student.code === code;
   });
 }
 
@@ -32,23 +40,21 @@ const generate_table = () => {
   table.appendChild(header);
   student_data.forEach(student => {
     const row_el = document.createElement("tr");
-    
+
     const name_col = document.createElement("td");
     name_col.textContent = student.name;
-    
+
     const class_col = document.createElement("td");
     class_col.textContent = student.class;
 
     const code_col = document.createElement("td");
     code_col.textContent = student.code;
 
-    
-
     const button_col = document.createElement("button");
     button_col.textContent = "Delete";
     button_col.name = student.code;
-    button_col.onclick = function () {delete_student(button_col.name)};
-    
+    button_col.onclick = function () { delete_student_controller(button_col.name) };
+
     row_el.appendChild(name_col);
     row_el.appendChild(class_col);
     row_el.appendChild(code_col);
@@ -59,38 +65,34 @@ const generate_table = () => {
 setTimeout(generate_table, 1);
 
 //Controller
-document.getElementById("teacher-logout").onclick = function () {location.href="../index.html"};
-
-const delete_student = (button_name) => {
-   student_data = student_data.filter(student => {
-     if (student.code === button_name) {
-       alert(student.name + " has been removed")
-       return false;
-     } else {
-       return true;
-     }
-   });
-   save_data("students", student_data);
-   setTimeout(generate_table, 1);
-} 
+document.getElementById("teacher-logout").onclick = function () { location.href = "../index.html" };
 
 const add_student_controller = () => {
   const student_name = document.getElementById("stu-fullname");
   const student_class = document.getElementById("add-class-name");
-  const student_code = generate_code();
-  if (student_exists(student_name.value)) {
-    alert("This student already exists in the system!")
-  } else {
-  add_student(student_name.value, student_class.value, student_code);
-    alert("Student successfully added!");
+  let student_code = generate_code();
+  while (code_exists(student_code)) {
+    student_code = generate_code();
   }
-  clear_textbox(student_name);
+
+  if (student_name.value === "" || student_class.value === "") {
+    alert("Student name / class cannot be empty!");
+  } else {
+    add_student(student_name.value, student_class.value, student_code);
+    clear_textbox(student_name);
+    setTimeout(generate_table, 1);
+  }
+}
+
+const delete_student_controller = (button_name) => {
+  delete_student(button_name);
+  setTimeout(generate_table, 1);
 }
 
 const generate_code = () => {
   let generated_code = "";
   var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  for(let i = 0; i < 3; i++){
+  for (let i = 0; i < 3; i++) {
     generated_code += alpha.charAt(Math.floor(Math.random() * alpha.length));
     generated_code += Math.floor(Math.random() * 10);
   }
